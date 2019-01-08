37a7f7f08ded58c0cdb2e0bfc3bad0a350eb6e52
801DevLabs <801DevLabs@gmail.com>
2019-01-08 06:59:48 -0700

    updated variables

--- JavaScript Language Fundamentals/Variables - var, let and const/app.js
+++ JavaScript Language Fundamentals/Variables - var, let and const/app.js
@@ -6,3 +6,48 @@ VARIABLES - VAR, LET AND CONST
 
 // var, let, const
 
+// var name = 'John Doe'
+// console.log(name)
+// name = 'Steve Smith'
+// console.log(name)
+
+// INIT A VARIABLE
+var greeting
+console.log(greeting)
+greeting = 'Hello'
+console.log(greeting)
+
+// LETTERS, NUMBERS, _, $
+// CANNOT START WITH NUMBER
+var $name = 'John'
+var _name = 'John'
+
+// MULTI WORD VARS
+var firstName = 'John' // CAMEL CASE
+var first_name = 'John' // UNDERSCORE
+var FirstName = 'John' // PASCAL CASE
+
+// LET
+// let name
+// name = 'John Doe'
+// console.log(name)
+// name = 'Steve Smith'
+// console.log(name)
+
+// CONST
+// const name = 'John'
+// name = 'Steve' // CANNOT REASSIGN
+// console.log(name)
+// const greeting // HAVE TO ASSIGN A VALUE
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Steve'
+person.age = '32'
+console.log(person)
+
+const numbers = [1,2,3,4,5]
+numbers.push(6)
+console.log(numbers)
