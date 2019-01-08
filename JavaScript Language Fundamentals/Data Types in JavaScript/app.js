58921cde0c034fb3d67f9cf1d8569b6c49f13c89
801DevLabs <801DevLabs@gmail.com>
2019-01-08 07:12:49 -0700

    updated data types

--- JavaScript Language Fundamentals/Data Types in JavaScript/app.js
+++ JavaScript Language Fundamentals/Data Types in JavaScript/app.js
@@ -6,5 +6,28 @@ DATA TYPES IN JAVASCRIPT
 
 // PRIMITIVE DATA TYPES
 
+// STRING
+const name = 'John Doe'
+console.log(typeof name)
+
+// NUMBER
+const age = 30
+console.log(typeof age)
+
+// BOOLEAN
+const hasKids = true
+console.log(typeof hasKids)
+
+// NULL
+const car = null
+console.log(typeof car)
+
+// UNDEFINED
+let test
+console.log(typeof test)
+
+// SYMBOL
+const sym = Symbol()
+console.log(typeof sym)
 
 // REFERENCE DATA TYPES
