92e0c1937d94cc99208b7e9f0a73459748b4fe07
801DevLabs <801DevLabs@gmail.com>
2019-01-09 12:02:33 -0700

    added arrays and array methods

--- JavaScript Language Fundamentals/Arrays and Array Methods/app.js
+++ JavaScript Language Fundamentals/Arrays and Array Methods/app.js
@@ -1,40 +1,82 @@
 /*
 ========================================
-Template Literals
+ARRAYS AND ARRAY METHODS
 ========================================
 */
 
-const name = 'Tom'
-const age = 28
-const job = 'Schedule Coordinator'
-const city = 'Pawnee'
-let html
+// CREATE SOME ARRAYS
+const numbers = [43,56,33,23,44,36,5]
+const numbers2 = new Array(22,45,33,76,54)
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear']
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:2}, new Date()]
 
-// WITHOUT TEMPLATE STRINGS (es5)
+let val
 
-html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>'
+// GET ARRAY LENGTH
+val = numbers.length
 
-html = '<ul>' +
-       '<li>Name: ' + name + '</li>' +
-       '<li>Age: ' + age + '</li>' +
-       '<li>Job: ' + job + '</li>' +
-       '<li>City: ' + city + '</li>'
+// CHECK IF IS ARRAY
+val = Array.isArray(numbers)
 
-function hello() {
-  return 'Hello'
+// GET A SINGLE VALUE
+val = numbers[3]
+val = numbers[0]
+
+// INSERT INTO ARRAY
+numbers[2] = 100
+
+// FIND INDEX OF VALUE
+val = numbers.indexOf(36)
+
+// ================
+// MUTATING ARRAYS
+// ================
+
+// ADD ONTO END
+numbers.push(250)
+
+// ADD ONTO FRONT
+numbers.unshift(120)
+
+// TAKE OFF FROM END
+numbers.pop()
+
+// TAKE OFF FROM FRONT
+numbers.shift()
+
+// SPLICE VALUES
+numbers.splice(1,3)
+
+// REVERSE
+numbers.reverse()
+
+// CONCATENATE ARRAY
+val = numbers.concat(numbers2)
+
+// SORTING ARRAYS
+val = fruit.sort()
+val = numbers.sort()
+
+// USE THE COMPARE FUNCTION
+val = numbers.sort(function(x, y){
+  return x - y
+})
+
+// REVERSE SORT
+val = numbers.sort(function(x, y){
+  return y - x
+})
+
+// FIND
+function under50(num) {
+  return num < 50
 }
 
-// WITH TEMPLATE STRINGS (es6)
-html = 
-`<ul>
-  <li>Name: ${name}</li>
-  <li>Age: ${age}</li>
-  <li>Job: ${job}</li>
-  <li>City: ${city}</li>
-  <li>${2+2}</li>
-  <li>${hello()}</li>
-  <li>${age >= 30 ? '30 or older' : 'Younger than 30'}</li>
-</ul>`
+function over50(num) {
+  return num > 50
+}
 
+val = numbers.find(over50)
 
-document.body.innerHTML = html
+console.log(numbers)
+console.log(val)
