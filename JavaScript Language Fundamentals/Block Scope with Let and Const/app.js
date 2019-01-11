5131cac20e8f1f47a51218700edf38068ebed952
801DevLabs <801DevLabs@gmail.com>
2019-01-11 09:01:59 -0700

    updated block scope with let and const

--- JavaScript Language Fundamentals/Block Scope with Let and Const/app.js
+++ JavaScript Language Fundamentals/Block Scope with Let and Const/app.js
@@ -10,21 +10,21 @@ let b = 2
 const c = 3
 
 // FUNCTION SCOPE
-// function test() {
-//   var a = 4
-//   let b = 5
-//   const c = 6
-//   console.log('Function Scope:', a, b, c)
-// }
-// test()
+function test() {
+  var a = 4
+  let b = 5
+  const c = 6
+  console.log('Function Scope:', a, b, c)
+}
+test()
 
 // IF SCOPE
-// if(true) {
-//   var a = 4
-//   let b = 5
-//   const c = 6
-//   console.log('If Scope:', a, b, c)
-// }
+if(true) {
+  var a = 4
+  let b = 5
+  const c = 6
+  console.log('If Scope:', a, b, c)
+}
 
 for(let a = 0; a < 10; a++){
   console.log(`Loop ${a}`)
