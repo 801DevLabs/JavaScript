a3c64aa2fbfea989fda1626616a9959b0c9248dc
801DevLabs <801DevLabs@gmail.com>
2019-01-17 06:30:57 -0700

    added event listeners and the event loop

---
+++ DOM Manipulation and Events/Event Listeners and the Event Loop/app.js
@@ -0,0 +1,44 @@
+/*
+====================================
+EVEN LISTENERS AND THE EVENT LOOP
+====================================
+*/
+
+// document.querySelector('.clear-tasks').addEventListener('click', function(e) {
+//   console.log('Hello World')
+
+//   e.preventDefault()
+// })
+
+document.querySelector('.clear-tasks').addEventListener('click', onClick)
+
+function onClick(e) {
+  // console.log('Clicked')
+
+  let val
+
+  val = e
+
+  // EVENT TARGET ELELEMENT
+  val = e.target
+  val = e.target.id
+  val = e.target.className
+  val = e.target.classList
+
+  // EVENT TYPE
+  val = e.type
+
+  // TIMESTAMP
+  val = e.timeStamp
+
+  // COORDS EVENT RELEATIVE TO THE WINDOW
+  val = e.claientY
+  val = e.clientX
+
+  // COORDS EVENT RELATIVE TO THE ELEMENT
+  val = e.offsetY
+  val = e.offsetX
+
+  console.log(val)
+  e.preventDefault()
+}
